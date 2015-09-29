6631fe4a92a0ab039dfb62213d5c8977793a64e7
phabian12 <wheelerdiw@gmail.com>
2015-09-29 13:51:38 +1000

    update for BookDAO... first 4 functions

--- src/library/daos/BookDAO.java
+++ src/library/daos/BookDAO.java
@@ -1,24 +1,69 @@
 package library.daos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import library.interfaces.daos.IBookDAO;
+import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 
 public class BookDAO implements IBookDAO{
 
+	private IBookHelper helper_;
+	private List<IBook> bookList_;
+	private int bookCounter_;
+	
+	//constructor
+	public BookDAO(IBookHelper helper){
+		//throws exception if helper is empty
+		if(helper == null){
+			throw new IllegalArgumentException("Helper Object cannot be null.");
+		}
+		else{
+			helper_ = helper;
+			bookList_ = new ArrayList<>();
+			bookCounter_ = 1;
+		}
+	}
+	
+	
+	//adding a new book
 	public IBook addBook(String author, String title, String callNo) {
+		IBook newBook = helper_.makeBook(author, title, callNo, bookCounter_++);
+		bookList_.add(newBook);
+		
+		return newBook;
+	}
+	
+	
+	//return book by it's unique ID
+	public IBook getBookByID(int bookID) {
+		for (IBook newBook : bookList_)
+			if(newBook.getID() == bookID){
+				return newBook;
+			}
 		return null;
 	}
-	public IBook getBookByID(int id) {
-		return null;
-	}
+	
+	
+	//return the list of books
 	public List<IBook> listBooks() {
-		return null;
+		return bookList_;
 	}
+	
+	
+	//returns a list of books written by an author
 	public List<IBook> findBooksByAuthor(String author) {
-		return null;
+		ArrayList<IBook> returnedBooks = new ArrayList<>();
+		for (IBook books : bookList_)
+			if(author.compareTo(books.getAuthor()) == 0){
+				returnedBooks.add(books);
+			}
+		return returnedBooks;
 	}
+	
+	
+	
 	public List<IBook> findBooksByTitle(String title) {
 		return null;
 	}
