0b4682c01876da3f5c560248c71b90c2809b43c0
phabian12 <wheelerdiw@gmail.com>
2015-09-29 12:28:43 +1000

    Updated BookDAO class

--- src/library/daos/BookDAO.java
+++ src/library/daos/BookDAO.java
@@ -1,5 +1,28 @@
 package library.daos;
 
-public class BookDAO {
+import java.util.List;
 
+import library.interfaces.daos.IBookDAO;
+import library.interfaces.entities.IBook;
+
+public class BookDAO implements IBookDAO{
+
+	public IBook addBook(String author, String title, String callNo) {
+		return null;
+	}
+	public IBook getBookByID(int id) {
+		return null;
+	}
+	public List<IBook> listBooks() {
+		return null;
+	}
+	public List<IBook> findBooksByAuthor(String author) {
+		return null;
+	}
+	public List<IBook> findBooksByTitle(String title) {
+		return null;
+	}
+	public List<IBook> findBooksByAuthorTitle(String author, String title) {
+		return null;
+	}
 }
