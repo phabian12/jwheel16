d4ebbfc19e20c1c29525d5262de0cb0d289b68c6
phabian12 <wheelerdiw@gmail.com>
2015-09-29 14:32:56 +1000

    finalized BookDAO

--- src/library/daos/BookDAO.java
+++ src/library/daos/BookDAO.java
@@ -27,7 +27,7 @@ public class BookDAO implements IBookDAO{
 	}
 	
 	
-	//adding a new book
+	//adding a new book to collection
 	public IBook addBook(String author, String title, String callNo) {
 		IBook newBook = helper_.makeBook(author, title, callNo, bookCounter_++);
 		bookList_.add(newBook);
@@ -36,7 +36,7 @@ public class BookDAO implements IBookDAO{
 	}
 	
 	
-	//return book by it's unique ID
+	//return book by it's unique ID from collection
 	public IBook getBookByID(int bookID) {
 		for (IBook newBook : bookList_)
 			if(newBook.getID() == bookID){
@@ -46,7 +46,7 @@ public class BookDAO implements IBookDAO{
 	}
 	
 	
-	//return the list of books
+	//return the list of books from collection
 	public List<IBook> listBooks() {
 		return bookList_;
 	}
@@ -54,20 +54,36 @@ public class BookDAO implements IBookDAO{
 	
 	//returns a list of books written by an author
 	public List<IBook> findBooksByAuthor(String author) {
-		ArrayList<IBook> returnedBooks = new ArrayList<>();
+		List<IBook> returnedBooks = new ArrayList<>();
 		for (IBook books : bookList_)
 			if(author.compareTo(books.getAuthor()) == 0){
 				returnedBooks.add(books);
 			}
+		//return empty list if no books found
 		return returnedBooks;
 	}
 	
 	
-	
+	//return list of titles by title
 	public List<IBook> findBooksByTitle(String title) {
-		return null;
+		List<IBook> returnedTitles = new ArrayList<IBook>();
+		for (IBook books : bookList_){
+			if (title.compareTo(books.getTitle()) == 0)
+				returnedTitles.add(books);
+		}
+		//returns empty list if no books found
+		return returnedTitles;
 	}
+	
+	
+	//return books with same author/title
 	public List<IBook> findBooksByAuthorTitle(String author, String title) {
-		return null;
+		List<IBook> results = new ArrayList<>();
+		for (IBook books : bookList_)
+			if (title.compareTo(books.getTitle()) == 0
+				&& author.compareTo(books.getAuthor()) == 0)
+					results.add(books);
+		//return empty list if no books found
+		return results;
 	}
 }
