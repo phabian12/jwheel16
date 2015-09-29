9876103195a70cbf07219c678a0d8f51ea715dac
phabian12 <wheelerdiw@gmail.com>
2015-09-29 14:39:49 +1000

    Finalized BookHelper

--- src/library/daos/BookHelper.java
+++ src/library/daos/BookHelper.java
@@ -1,5 +1,13 @@
 package library.daos;
 
-public class BookHelper {
+import library.entities.Book;
+import library.interfaces.daos.IBookHelper;
+import library.interfaces.entities.IBook;
 
+public class BookHelper implements IBookHelper{
+
+	public IBook makeBook(String author, String title, String callNumber, int id) {
+		
+		return new Book(author, title, callNumber, id);
+	}
 }
