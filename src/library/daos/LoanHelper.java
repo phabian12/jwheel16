398d6cc9d90e695a376d75f5f195e461d8c85832
phabian12 <wheelerdiw@gmail.com>
2015-09-30 18:34:18 +1000

    implemented Dr. Tulip's LoanHelper.java

--- src/library/daos/LoanHelper.java
+++ src/library/daos/LoanHelper.java
@@ -1,5 +1,19 @@
 package library.daos;
 
-public class LoanHelper {
+import java.util.Date;
 
+import library.entities.Loan;
+import library.interfaces.daos.ILoanHelper;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+public class LoanHelper implements ILoanHelper {
+
+	@Override
+	public ILoan makeLoan(IBook book, IMember borrower, Date borrowDate,
+			Date dueDate) {
+
+		return new Loan(book, borrower, borrowDate, dueDate);
+	}
 }
