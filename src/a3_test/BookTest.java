0a63b5113ed2db3a0b7d96c3b584015afab86c7b
phabian12 <wheelerdiw@gmail.com>
2015-09-30 20:59:19 +1000

    BookTest.java reading for testing

--- src/a3_test/BookTest.java
+++ src/a3_test/BookTest.java
@@ -1,5 +1,77 @@
 package a3_test;
 
-public class BookTest {
+import library.interfaces.entities.EBookState;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import static org.mockito.Mockito.*;
+
+
+public class BookTest implements IBook{
+
+	@Override
+	public void borrow(ILoan loan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ILoan getLoan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void returnBook(boolean damaged) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void lose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void repair() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EBookState getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCallNumber() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
