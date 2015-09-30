e362daab68e0e3249ec954489e75f67fe0df4575
phabian12 <wheelerdiw@gmail.com>
2015-09-30 19:08:39 +1000

    Implemented Dr. Tulip's MemberHelper.java

--- src/library/daos/MemberHelper.java
+++ src/library/daos/MemberHelper.java
@@ -5,12 +5,12 @@ import library.interfaces.daos.IMemberHelper;
 import library.interfaces.entities.IMember;
 
 public class MemberHelper implements IMemberHelper {
-
-	@Override
+	
+	
+	
 	public IMember makeMember(String firstName, String lastName,
 			String contactPhone, String emailAddress, int id) {
 		
 		return new Member(firstName, lastName, contactPhone, emailAddress, id);
 	}
-
 }
