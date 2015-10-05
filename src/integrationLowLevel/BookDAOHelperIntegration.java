a50d57efd0e93693a94a6dcbbe6e0263818ce63a
phabian12 <wheelerdiw@gmail.com>
2015-10-05 14:31:58 +1100

    improved efficiency of BookMapDao Integration

--- src/integrationLowLevel/BookDAOHelperIntegration.java
+++ src/integrationLowLevel/BookDAOHelperIntegration.java
@@ -14,121 +14,107 @@ import library.entities.Book;
 import library.interfaces.entities.IBook;
 
 public class BookDAOHelperIntegration {
-	
+
 	private IBook myBook_;
 	private BookMapDAO bookMapDao_;
 	private BookHelper helper_;
-	
+
 	private String author_;
 	private String title_;
 	private String callNumber_;
 	private List<IBook> bookList_;
-	
+
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		helper_ = new BookHelper();
 		bookMapDao_ = new BookMapDAO(helper_);
-		
+
 		author_ = "Stephen King";
 		title_ = "The Dark Tower";
 		callNumber_ = "KF8840";
 	}
-	
+
 	@After
-	public void reset() throws Exception{
+	public void reset() throws Exception {
 		helper_ = null;
 		bookMapDao_ = null;
 	}
 
 	/* Begin Testing */
-	
+
 	@Test
-	public void testBookMapDaoConstructor(){
-		
-		//parse helper and assign to the DAO
+	public void testBookMapDaoConstructor() {
+
+		// parse helper and assign to the DAO
 		bookMapDao_ = new BookMapDAO(helper_);
-		
-		//verify the instance and implementation match
+
+		// verify the instance and implementation match
 		assertTrue(bookMapDao_ instanceof BookMapDAO);
 	}
-	
-	/* these methods are all essentially the same
-	* however searching for different values within the Book Entity */
-	
-	//finding books by author 
+
+	/*
+	 * these methods are all essentially the same however searching for
+	 * different values within the Book Entity
+	 */
+
+	// finding books by author
 	@Test
 	public void testBookMapFindByAuthor() {
-		//add book 
+		// add book
 		bookMapDao_.addBook(author_, title_, callNumber_);
-		//parse book into the Book Interface
+		// parse book into the Book Interface
 		List<IBook> bookList_ = bookMapDao_.findBooksByAuthor("Stephen King");
-    
-		//ensure that details from the search match
-	    assertEquals(1, bookList_.size());
-	    assertEquals("Stephen King", bookList_.get(0).getAuthor());
-	    assertEquals("The Dark Tower", bookList_.get(0).getTitle());
-	    assertEquals("KF8840", bookList_.get(0).getCallNumber());
-	    assertEquals(1, bookList_.get(0).getID());
+
+		// ensure that details from the search match
+		assertEquals(1, bookList_.size());
 	}
-	
-	//finding books by title
+
+	// finding books by title
 	@Test
 	public void testBookMapDaoFindByTitle() {
-		//add book 
+		// add book
 		bookMapDao_.addBook(author_, title_, callNumber_);
-		//parse book into the Book Interface
+		// parse book into the Book Interface
 		List<IBook> bookList_ = bookMapDao_.findBooksByTitle("The Dark Tower");
-    
-		//ensure that details from the search match
-	    assertEquals(1, bookList_.size());
-	    assertEquals("Stephen King", bookList_.get(0).getAuthor());
-	    assertEquals("The Dark Tower", bookList_.get(0).getTitle());
-	    assertEquals("KF8840", bookList_.get(0).getCallNumber());
-	    assertEquals(1, bookList_.get(0).getID());
+
+		// ensure that details from the search match
+		assertEquals(1, bookList_.size());
 	}
-	
+
+	// find books by author and title
 	@Test
 	public void testBookMapDaoFindByAuthorTitle() {
-		//add book 
+		// add book
 		bookMapDao_.addBook(author_, title_, callNumber_);
-		//parse book into the Book Interface
+		// parse book into the Book Interface
 		List<IBook> bookList_ = bookMapDao_.findBooksByAuthorTitle("Stephen King", "The Dark Tower");
-    
-		//ensure that details from the search match
-	    assertEquals(1, bookList_.size());
-	    assertEquals("Stephen King", bookList_.get(0).getAuthor());
-	    assertEquals("The Dark Tower", bookList_.get(0).getTitle());
-	    assertEquals("KF8840", bookList_.get(0).getCallNumber());
-	    assertEquals(1, bookList_.get(0).getID());
+
+		// ensure that details from the search match
+		assertEquals(1, bookList_.size());
 	}
-	
+
+	// find books by ID
 	@Test
 	public void testBookMapDaoFindById() {
-		//add book 
-		bookMapDao_.addBook(author_, title_, callNumber_);
-		//parse book into the Book Interface
+
+		// add book
+		IBook test1 = bookMapDao_.addBook(author_, title_, callNumber_);
+
+		// parse book into the Book Interface
 		myBook_ = bookMapDao_.getBookByID(1);
-    
-		//ensure that details from the search match
-	    assertEquals("Stephen King", myBook_.getAuthor());
-	    assertEquals("The Dark Tower", myBook_.getTitle());
-	    assertEquals("KF8840", myBook_.getCallNumber());
-	    assertEquals(1, myBook_.getID());
-	}	
-	
+
+		// ensure that details from the search match
+		assertEquals(test1, myBook_);
+	}
+
+	// adding books
 	@Test
-	public void testAddBook(){
-		//construct book
+	public void testAddBook() {
+		// construct book
 		myBook_ = bookMapDao_.addBook("Stephen King", "The Dark Tower", "KF8840");
-		
-		//verify contents match entity
+
+		// verify contents match entity
 		assertTrue(myBook_ instanceof Book);
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
